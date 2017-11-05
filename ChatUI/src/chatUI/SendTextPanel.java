64ead0ad2a9fd1bcf37b9efb0ae3ce60a784a830
Coco Cao Jinglu <jinglucao@u.yale-nus.edu.sg>
2017-11-05 19:22:22 +0800

    Basic structure of chatUI created

---
+++ ChatUI/src/chatUI/SendTextPanel.java
@@ -0,0 +1,16 @@
+package chatUI;
+
+import javax.swing.JPanel;
+
+import chatUI.MainWindow;
+
+public class SendTextPanel extends JPanel 
+{
+	private MainWindow mw;
+
+	public SendTextPanel(MainWindow mw) {
+		// TODO Auto-generated constructor stub
+		this.mw = mw;
+	}
+
+}
---
+++ ChatUI/src/chatUI/ShowTextPanel.java
@@ -0,0 +1,16 @@
+package chatUI;
+
+import javax.swing.JPanel;
+
+import chatUI.MainWindow;
+
+public class ShowTextPanel extends JPanel 
+{
+	private MainWindow mw;
+
+	public ShowTextPanel(MainWindow mw) {
+		// TODO Auto-generated constructor stub
+		this.mw = mw;
+	}
+
+}
