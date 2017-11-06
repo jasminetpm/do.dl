74b8c9f4a1efc07a9a3923a76e2d86d8f103253b
Coco Cao Jinglu <jinglucao@u.yale-nus.edu.sg>
2017-11-06 14:44:58 +0800

    SendTextPanel updated

--- ChatUI/src/chatUI/SendTextPanel.java
+++ ChatUI/src/chatUI/SendTextPanel.java
@@ -1,16 +1,31 @@
 package chatUI;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import com.sun.xml.internal.txw2.Document;
 
 import chatUI.MainWindow;
 
 public class SendTextPanel extends JPanel 
 {
 	private MainWindow mw;
+	private JButton sendButton;
+	private JTextArea jta;
 
 	public SendTextPanel(MainWindow mw) {
 		// TODO Auto-generated constructor stub
 		this.mw = mw;
+		this.setLayout(new BorderLayout());
+		this.sendButton = new JButton("Send");
+		this.jta = new JTextArea(7, 15);
+		this.jta.setBackground(Color.WHITE);
+		this.add(this.sendButton, BorderLayout.EAST);
+		this.add(this.jta, BorderLayout.WEST);
 	}
 
 }
