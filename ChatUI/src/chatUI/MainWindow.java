1b3534850955892c13dc3aef4bd4b2ec925b7ae3
Coco Cao Jinglu <jinglucao@u.yale-nus.edu.sg>
2017-11-06 14:19:41 +0800

    Last time comp crashed failed to save anything for MainWindow. so redid it (also previous versions won't work before of the failure to save due to stupid computer)

--- ChatUI/src/chatUI/MainWindow.java
+++ ChatUI/src/chatUI/MainWindow.java
@@ -1,38 +1,33 @@
 package chatUI;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 
-import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 
-import chatUI.MainWindow;
-import chatUI.ShowTextPanel;
-import chatUI.SendTextPanel;
-import chatUI.TitlePanel;
 
 public class MainWindow extends JFrame
 {
-	private ShowTextPanel stp;
-	private SendTextPanel tbp;
+	private SendTextPanel sendp;
+	private ShowTextPanel showp;
 	private TitlePanel tp;
-	
 	
 	public MainWindow()
 	{
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLayout(new BoxLayout(this.getContentPane(), BoxLayout.Y_AXIS));
-		this.stp = new ShowTextPanel(this);
-		this.tbp = new SendTextPanel(this);
 		this.tp = new TitlePanel(this);
-		this.stp.setAlignmentX(CENTER_ALIGNMENT);
-		this.add(this.tp);
-		this.add(this.stp);
-		this.add(this.tbp);
+		this.showp = new ShowTextPanel(this);
+		this.sendp = new SendTextPanel(this);
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		
+		this.getContentPane().add(this.tp, BorderLayout.PAGE_START);
+		this.getContentPane().add(this.showp, BorderLayout.CENTER);
+		this.getContentPane().add(this.sendp,BorderLayout.PAGE_END);
 		this.setMinimumSize(new Dimension(260,540));
 		this.pack();
 		this.setVisible(true);
 	}
-	
 	
 	public static void main (String args[])
 	{
