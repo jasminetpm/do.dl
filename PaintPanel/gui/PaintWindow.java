11cdb0cb63f402b107f9f3383a7ce52538caf543
Jaime Koh <jkmingwen@gmail.com>
2017-11-27 23:17:34 +0800

    Fixed problem of double dialog box.

--- PaintPanel/gui/PaintWindow.java
+++ PaintPanel/gui/PaintWindow.java
@@ -31,6 +31,7 @@ import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.imageio.ImageIO;
@@ -378,7 +379,7 @@ public class PaintWindow extends JFrame {
 	}
 	
 	public String getCommentText() {
-		return this.commentEntry.getText();
+		return JOptionPane.showInputDialog("Enter comment here:");
 	}
 	
 	public ArrayList<CommentInstruction> getCommentList() {
@@ -411,8 +412,8 @@ public class PaintWindow extends JFrame {
 		if (this.currentCommentInstruction == null) {
 			System.out.println("Please drag a circle to indicate where you are commenting on");
 		} else {
-			this.currentCommentInstruction.setCommentText(this.commentEntry.getText());
-			if (this.currentCommentInstruction.getCommentText().equals("")) {
+			this.currentCommentInstruction.setCommentText(this.currentCommentInstruction.getComment());
+			if (this.currentCommentInstruction.getComment().equals("")) {
 				System.out.println("Please enter your comment");
 			} else {
 				this.commentIndex++;
